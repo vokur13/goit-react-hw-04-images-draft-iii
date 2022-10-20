1efd9c8abb127b39d5a03d130b3b24ffa516af53
vokur13 <vokur13@gmail.com>
2022-10-20 15:33:42 +0300

    under construction

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,50 +1,85 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'css/styles.css';
 import { Searchbar } from 'components/Searchbar';
 import { ImageGalleryHub } from 'components/ImageGalleryHub';
 
-export class App extends Component {
-  static defaultProps = {
-    initialValue: 1,
-  };
-  state = {
-    page: this.props.initialValue,
-    query: '',
-    gallery: [],
-    total: null,
-    totalHits: null,
-  };
+export const App = () => {
+  const initialValue = 1;
 
-  handleFormSubmit = ({ query }) => {
-    const q = query.trim().toLowerCase();
-    if (q === '') {
+  const [page, setPage] = useState(initialValue);
+  const [query, setQuery] = useState('');
+  const [gallery, setGallery] = useState([]);
+  const [total, setTotal] = useState(null);
+  const [totalHits, setTotalHits] = useState(null);
+
+  function handleFormSubmit({ query }) {
+    if (!query.trim().toLowerCase()) {
       return toast.warn('Please let us know your query item');
     }
-    this.setState({
-      page: this.props.initialValue,
-      query: q,
-      gallery: [],
-      total: null,
-      totalHits: null,
-    });
-  };
-
-  render() {
-    const { page, query, gallery, total, totalHits } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGalleryHub
-          page={page}
-          query={query}
-          gallery={gallery}
-          total={total}
-          totalHits={totalHits}
-        />
-        <ToastContainer position="top-left" autoClose={5000} />
-      </>
-    );
+    setPage(initialValue);
+    setQuery(query.trim().toLowerCase());
+    setGallery([]);
+    setTotal(null);
+    setTotalHits(null);
   }
-}
+
+  return (
+    <>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGalleryHub
+        page={page}
+        query={query}
+        gallery={gallery}
+        total={total}
+        totalHits={totalHits}
+      />
+      <ToastContainer position="top-left" autoClose={5000} />
+    </>
+  );
+};
+
+// export class protoApp extends Component {
+//   static defaultProps = {
+//     initialValue: 1,
+//   };
+//   state = {
+//     page: this.props.initialValue,
+//     query: '',
+//     gallery: [],
+//     total: null,
+//     totalHits: null,
+//   };
+
+//   handleFormSubmit = ({ query }) => {
+//     const q = query.trim().toLowerCase();
+//     if (q === '') {
+//       return toast.warn('Please let us know your query item');
+//     }
+//     this.setState({
+//       page: this.props.initialValue,
+//       query: q,
+//       gallery: [],
+//       total: null,
+//       totalHits: null,
+//     });
+//   };
+
+//   render() {
+//     const { page, query, gallery, total, totalHits } = this.state;
+//     return (
+//       <>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+//         <ImageGalleryHub
+//           page={page}
+//           query={query}
+//           gallery={gallery}
+//           total={total}
+//           totalHits={totalHits}
+//         />
+//         <ToastContainer position="top-left" autoClose={5000} />
+//       </>
+//     );
+//   }
+// }
